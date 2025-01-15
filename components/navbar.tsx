02273adc23515e94a56f5e550aad5491bd764450
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2025-01-15 11:55:54 -0600

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,61 +1,90 @@
 "use client";
 import {
-  Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarBrand,
-  NavbarItem,
+    Navbar as NextUINavbar,
+    NavbarContent,
+    NavbarBrand,
+    NavbarItem, NavbarMenuToggle,
+    NavbarMenuItem,
+    NavbarMenu,
 } from "@nextui-org/navbar";
-import { Link } from "@nextui-org/link";
-import { link as linkStyles } from "@nextui-org/theme";
+import {Link} from "@nextui-org/link";
+import {link as linkStyles} from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
-import { Image } from "@nextui-org/image";
+import {Image} from "@nextui-org/image";
+import React from "react";
 
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import { TwitterIcon, GithubIcon } from "@/components/icons";
+import {siteConfig} from "@/config/site";
+import {ThemeSwitch} from "@/components/theme-switch";
+import {TwitterIcon, GithubIcon} from "@/components/icons";
 
 export const Navbar = () => {
-  return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Image alt="Logo" className="h-26 w-32 mt-5" src="/logo2.png" />
-          </NextLink>
-        </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-      </NavbarContent>
-    </NextUINavbar>
-  );
+    return (
+
+        <NextUINavbar maxWidth="xl" position="sticky" onMenuOpenChange={setIsMenuOpen}>
+            <NavbarContent>
+                <NavbarMenuToggle
+                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                    className="md:hidden"
+                />
+                <NavbarBrand>
+                    <NextLink className="flex justify-start items-center gap-1" href="/">
+                        <Image alt="Logo" className="h-26 w-32 mt-5" src="/logo2.png"/>
+                    </NextLink>
+                </NavbarBrand>
+            </NavbarContent>
+
+
+            <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+                <ul className="hidden lg:flex gap-4 justify-start ml-2">
+                    {siteConfig.navItems.map((item) => (
+                        <NavbarItem key={item.href}>
+                            <NextLink
+                                className={clsx(
+                                    linkStyles({color: "foreground"}),
+                                    "data-[active=true]:text-primary data-[active=true]:font-medium",
+                                )}
+                                color="foreground"
+                                href={item.href}
+                            >
+                                {item.label}
+                            </NextLink>
+                        </NavbarItem>
+                    ))}
+                </ul>
+            </NavbarContent>
+
+            <NavbarMenu>
+                {siteConfig.navItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <NextLink
+                            className={clsx(
+                                linkStyles({color: "foreground"}),
+                                "data-[active=true]:text-primary data-[active=true]:font-medium",
+                            )}
+                            href={item.href}>
+                            {item.label}
+                        </NextLink>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+
+            <NavbarContent
+                className="sm:flex basis-1/5 sm:basis-full"
+                justify="end"
+            >
+                <NavbarItem className="sm:flex gap-2">
+                    <Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>
+                        <TwitterIcon className="text-default-500"/>
+                    </Link>
+                    <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+                        <GithubIcon className="text-default-500"/>
+                    </Link>
+                    <ThemeSwitch/>
+                </NavbarItem>
+            </NavbarContent>
+        </NextUINavbar>
+    );
 };
